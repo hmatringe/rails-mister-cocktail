ff0a96b3ad37e13a625130558666d223a073cd6a
Matringe <hadrien.matringe@gmail.com>
2017-05-19 07:07:52 +0200

    Working models and controllers, added headers and footers work in progress

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,13 +1,33 @@
 class DosesController < ApplicationController
-  def index
-  end
 
   def new
+    # @cocktail = Cocktail.find(params[:cocktail_id])
+    # @ingredient = Ingredient.new
+    # @dose = Dose.new
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render "cocktails/show"
+    end
   end
 
-  def show
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = Cocktail.find(@dose.cocktail_id)
+    @dose.destroy
+    redirect_to @cocktail
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:id,:description,:cocktail_id,:ingredient_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,11 @@
 Rails.application.routes.draw do
-  get 'doses/index'
 
-  get 'doses/new'
-
-  get 'doses/create'
-
-  get 'doses/show'
-
-  get 'ingredients/index'
-
-  get 'ingredients/new'
-
-  get 'ingredients/create'
-
-  get 'ingredients/show'
-
-  get 'cocktails/index'
-
-  get 'cocktails/new'
-
-  get 'cocktails/create'
-
-  get 'cocktails/show'
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
   root to: 'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
+
