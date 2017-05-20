a77f3a6f737bf81e06d21e350784ffcfb898cc3f
Matringe <hadrien.matringe@gmail.com>
2017-05-20 22:12:35 +0200

    updated seed to load ingredients from http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,17 @@
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "White rum")
-Ingredient.create(name: "Simple syrup")
-Ingredient.create(name: "Lime juice")
+require 'json'
+require 'open-uri'
+
+Ingredient.create(name:"test")
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts 'Creating ingredients...'
+api_ingredients = {}
+api_ingredients = JSON.parse(open("http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read)
+
+api_ingredients["drinks"].each do |x|
+  Ingredient.create(name:"#{x["strIngredient1"]}")
+end
+
+# api_ingredients["drinks"][0]["strIngredient1"]
